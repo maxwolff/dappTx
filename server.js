6b6fb1e97645c7e05ad47972a0c6945b21f4b27d
max wolff <max3@stanford.edu>
2018-01-29 15:55:09 -0800

    typo in port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-  app = express(),
+  app = express()
   //port = process.env.PORT || 3000;
-  const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('public'));
 
