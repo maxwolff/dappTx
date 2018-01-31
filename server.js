517b380d47a3dc83b98f24dc5f8828d66913cd8e
max wolff <max3@stanford.edu>
2018-01-30 22:56:02 -0800

    edit PORT
    add dotenv dependency

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
-
-
-/* const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true })); */
-//let default_server_port = 8000 // react is 8000
-let express = require('express'),
-  app = express(),
-  port = process.env.PORT || 5000; // heroku is 5000
-app.use(express.static('public'));
 require('dotenv').config()
-
+let express = require('express'),
+  app = express()
+const PORT = process.env.PORT || 5000
+app.use(express.static('public'));
 
 let query = require('./frequency.js');
 
-
- 
 async function getData(req,res){
 	var result = await query.getFreq(req.params['contractID'], req.params['startTime'],req.params['endTime']) 
   res.send(result)
 }
-
 
 app.get('/api/:contractID/:startTime/:endTime', function (req, res) {
   getData(req,res)
 })
 
 app.get('/', function (req, res) {
-  res.send("hi")
+  res.send("'/api/:contractID/:startTime/:endTime' , all three in hex")//test fucntion
 })
 
-app.get('/api', function (req, res) {
-  res.send("'/api/:contractID/:startTime/:endTime' all hex")//test fucntion
-})
-
-app.listen(port, function () {
-  console.log(`Listening on ${port}`)
+app.listen(PORT, function () {
+  console.log(`Listening on ${PORT}`)
 })
 
