dbe4e2689a964e121f552879a797079af70d8533
max wolff <max3@stanford.edu>
2018-02-15 20:57:22 -0700

    refactor query functions to combine sql, speed responses.

--- sql/makeTable.sql
+++ sql/makeTable.sql
@@ -1,2 +1,3 @@
 CREATE TABLE transactions(blob jsonb);
 CREATE INDEX ON transactions((blob->>'to'));
+CREATE INDEX ON transactions((blob->>'timestamp'));
--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 module.exports.sumObjectsByKey = sumObjectsByKey;
-//module.exports.get = get;
+module.exports.get = get;
+const request = require('async-request');
 
 const saveFile = (fileName, data) => { // duplicated func from pull data, got to clean this up and throw it in a util.js
 	fs.writeFile(fileName, JSON.stringify(data,null), (err) => {
@@ -19,3 +20,12 @@ function sumObjectsByKey(...objs) {
     return a;
   }, {});
 }
+
+
+async function get(url) {
+  try {
+    return await request(url);
+  } catch (e) {
+    throw e;
+  }
+};
