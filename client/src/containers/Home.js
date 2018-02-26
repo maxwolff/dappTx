28e648522631b44893a2ff9ea027dadd0e3925da
max wolff <max3@stanford.edu>
2018-02-25 16:07:35 -0800

    added functions graph

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -6,11 +6,11 @@ import axios from 'axios';
 
 
 class Home extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      config: {}
+      config1: {},
+      config2: {}
     };
   }
 
@@ -21,53 +21,78 @@ class Home extends Component {
   }
 
   callApi = async () => {
-    axios.get('/api/0x8d12a197cb00d4747a1fe03395095ce2a5cc6819/0x5a6fc956/0x5A8F969F') // hard coded
+    axios.get('/api/0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/0x5A1340E0/0x5A8F969F') // hard coded
   .then((response) => {
     var data = response.data
-    var sampled = [];
+    //var sampled = [];
     var contracts = []; 
-    console.log('data',data)
+    var funcList = []; 
+    var funcResultArr = [];
     Object.keys(data).forEach( elem => {
       var dateTemp = new Date(elem)
       //sampled.push([Math.round(dateTemp.getTime()),data[elem]['sampledEthTx']])
-      var epoch =Math.round(dateTemp.getTime())
-      var perc = 1.0 * data[elem]['contractTx'] / data[elem]['sampledEthTx']
+      var epoch = Math.round(dateTemp.getTime())
+      var perc = 100.0 * data[elem]['contractTx'] / data[elem]['sampledEthTx'] 
       contracts.push([epoch, perc])
+
+      var elemFuncs = data[elem].functions
+      Object.keys(elemFuncs).forEach( func => {
+        if (!(funcList.includes(func))){
+          funcList.push(func)
+          funcResultArr[func] = [];
+        }else{
+          funcResultArr[func].push([epoch,data[elem].functions[func]])
+        }
+      });
     });
     this.setState({
-      config: {
+      config1: {
         title: {
-          text: 'Etherdelta as % of Ethereum Transactions'
+          text: 'Contract usage as % of Sampled Ethereum Transactions'
         },
         series: [
         {
           data: contracts, 
-          name: 'percent',
+          name: '%',
            tooltip: {
-                valueDecimals: 2
+                valueDecimals: 2,
+                xDateFormat: '%Y-%m-%d'
             }
-        }
-        ]
+
+
+        }]
+      }
+    });
+    var result = []
+    for (var i in funcResultArr){
+      result.push({
+        data: funcResultArr[i],
+        name: i, 
+        tooltip:{ xDateFormat: '%Y-%m-%d'}
+      })
+    }
+    this.setState({
+      config2: {
+        title: {
+          text: 'Contract Function Call Usage'
+        },
+        series: result
       }
     })
+    console.log(this.state)
   }).catch(function (error) {
     console.log(error);
     });
   };
 
-
-/*
-var g = new Date()
-g = g/1000
-g.toString(16)
-parse int or smtg 
-*/
-
-
   render() {
     return (
-      <MyStockChart config={this.state.config} />
+    <div>
+      <MyStockChart config={this.state.config1} />,
+      <MyStockChart config={this.state.config2} />
+    </div>
     )
   }
 }
 export default Home;
+
