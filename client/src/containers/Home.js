31ff92f6ef50bde19aedc2df2979ce12155e6955
Stedman Halliday <stedman.rh@gmail.com>
2018-04-03 14:20:28 -0700

    comment for refactor; change component order

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -24,81 +24,87 @@ class Home extends Component {
 
   callApi = async () => {
     const url = '/api/' + this.state.address + '/0x5A1340E0/0x5A8F969F';  //'/api/0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/0x5A1340E0/0x5A8F969F'
-    console.log(url)
-    axios.get(url).then((response) => {
-    var data = response.data
-    //var sampled = [];
-    var contracts = []; 
-    var funcList = []; 
-    var funcResultArr = [];
-    Object.keys(data).forEach( elem => {
-      var dateTemp = new Date(elem)
-      //sampled.push([Math.round(dateTemp.getTime()),data[elem]['sampledEthTx']])
-      var epoch = Math.round(dateTemp.getTime())
-      var perc = 100.0 * data[elem]['contractTx'] / data[elem]['sampledEthTx'] 
-      contracts.push([epoch, perc])
+    //console.log(url)
 
-      var elemFuncs = data[elem].functions
-      Object.keys(elemFuncs).forEach( func => {
-        if (!(funcList.includes(func))){
-          funcList.push(func)
-          funcResultArr[func] = [];
-        }else{
-          funcResultArr[func].push([epoch,data[elem].functions[func]])
+    axios.get(url).then((response) => {   //call API with URL to get data | init arrays for data
+      let data = response.data
+      //var sampled = [];
+      let contracts = []; 
+      let funcList = []; 
+      let funcResultArr = [];
+
+      Object.keys(data).forEach( elem => {  //manipulate contract data and push to contracts array
+        const dateTemp = new Date(elem);
+        //sampled.push([Math.round(dateTemp.getTime()),data[elem]['sampledEthTx']])
+        const epoch = Math.round(dateTemp.getTime());
+        const perc = 100.0 * data[elem]['contractTx'] / data[elem]['sampledEthTx']; 
+        contracts.push([epoch, perc]);
+
+        const elemFuncs = data[elem].functions;   //manipulate function list and usage data and push to function list and usage arrays
+        Object.keys(elemFuncs).forEach( func => {
+          if (!(funcList.includes(func))){
+            funcList.push(func)
+            funcResultArr[func] = [];
+          }else{
+            funcResultArr[func].push([epoch,data[elem].functions[func]])
+          }
+        });
+      });
+
+      this.setState({   //doing too much-async causing render issues? manipulate config data, THEN call setState
+        config1: {
+          title: {
+            text: 'Contract usage as % of Sampled Ethereum Transactions'
+          },
+          series: [
+          {
+            data: contracts, 
+            name: '%',
+            tooltip: {
+                  valueDecimals: 2,
+                  xDateFormat: '%Y-%m-%d'
+              }
+
+
+          }]
         }
       });
-    });
-    this.setState({
-      config1: {
-        title: {
-          text: 'Contract usage as % of Sampled Ethereum Transactions'
-        },
-        series: [
-        {
-          data: contracts, 
-          name: '%',
-           tooltip: {
-                valueDecimals: 2,
-                xDateFormat: '%Y-%m-%d'
-            }
 
+      var result = []   //manipulate function usage array for chart config
+      for (var i in funcResultArr){
+        result.push({
+          data: funcResultArr[i],
+          name: i, 
+          tooltip:{ xDateFormat: '%Y-%m-%d'}
+        });
+      }
 
-        }]
-      }
-    });
-    var result = []
-    for (var i in funcResultArr){
-      result.push({
-        data: funcResultArr[i],
-        name: i, 
-        tooltip:{ xDateFormat: '%Y-%m-%d'}
-      })
-    }
-    this.setState({
-      config2: {
-        title: {
-          text: 'Contract Function Call Usage'
-        },
-        series: result
-      }
-    })
-    console.log(this.state)
-  }).catch(function (error) {
-    console.log(error);
+      this.setState({   //state should probably be set after API is called, data is manipulated, and configs are ready
+        config2: {
+          title: {
+            text: 'Contract Function Call Usage'
+          },
+          series: result
+        }
+      });
+
+      //console.log(this.state);
+    }).catch(function (error) {
+      console.log(error);
     });
   };
 
-  renderAddress = (submitData) => {
-    this.setState({address: submitData})
-    this.callApi()
+  renderAddress = (submitData) => {   //change Tx address in container state, call API, should change state independently of API call
+    this.setState({address: submitData});
+    this.callApi();
   } 
 
   render() {
     return (
     <div>
-      <MyStockChart config={this.state.config1} />,
-      <MyStockChart config={this.state.config2} />, 
       <NameForm addressCallback = {this.renderAddress}/>
+      <MyStockChart config={this.state.config1} />
+      <MyStockChart config={this.state.config2} /> 
     </div>
     )
   }
