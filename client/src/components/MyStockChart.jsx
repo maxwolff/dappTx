307220c6a13eb7472b7d5028dfd0c40706fa814f
Stedman Halliday <stedman.rh@gmail.com>
2018-04-04 14:47:59 -0700

    animations w/ reflow 🔥

--- client/src/components/MyStockChart.jsx
+++ client/src/components/MyStockChart.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 const ReactHighstock = require('react-highcharts/ReactHighstock.src')
 //var Highlight = require('react-highlight')
+let chartReflow = undefined
 
 class MyStockChart extends React.Component {
 
   componentDidUpdate() {
-		let chart = this.refs.chart.getChart();
-		chart.reflow = () => {};
+		const chart = this.refs.chart ? this.refs.chart.getChart() : {}
+    chartReflow = chartReflow || chart.reflow
+    chart.reflow = () => {}
+    setTimeout(() => (chart.reflow = chartReflow))
 	}
 
   render(){
