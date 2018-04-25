a9ce74e2302ec7173236eb9fc790569b432e9d8b
Stedman Halliday <stedman.rh@gmail.com>
2018-04-24 20:07:45 -0700

    prototype chart components

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -8,16 +8,62 @@ class Home extends Component {
     super(props)
 
     this.contractConfig = {   //init config for contract usage chart
+      chart: {
+        borderColor: '#e6e6e8',
+        borderWidth: 1,
+        borderRadius: 2,
+        spacing: [16,16,16,16]
+      },
+      colors: ['#54A0FF'],
       loading: {
         showDuration: 500,
         labelStyle: {
-          color: '#0080ff',
           fontFamily: 'IBM Plex Mono, sans-serif',
-          fontWeight: '400'
+          fontWeight: 400,
+          color: '#383840'
         }
       },
-      title: {
-        text: 'Contract Txns (Pct. of Sampled Ethereum Txns)'
+      rangeSelector: {
+        buttonTheme: {
+          fill: '#f6f6f8',
+          style: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+          },
+          states: {
+            hover: {
+              fill: '#0080ff',
+              style: {
+                color: 'white'
+              }
+            },
+            select: {
+              fill: '#54A0FF',
+              r: 1,
+              style: {
+                color: 'white'
+              }
+            }
+          }
+        },
+        inputStyle: {
+          fontFamily: 'IBM Plex Mono, sans-serif',
+          color: '#383840'
+        },
+        inputBoxBorderColor: '#e6e6e8',
+        labelStyle: {
+          fontFamily: 'IBM Plex Mono, sans-serif',
+          color: '#383840'
+        }
+      },
+      scrollbar: {
+        barBackgroundColor: '#f6f6f8',
+        barBorderColor: 'e6e6e8',
+        buttonArrowColor: '#383840',
+        buttonBackgroundColor: '#f6f6f8',
+        buttonBorderColor: '#e6e6e8',
+        trackBackgroundColor: '#e6e6e8',
+        trackBorderColor: '#e6e6e8'
       },
       series: [
       {
@@ -28,17 +74,100 @@ class Home extends Component {
               valueSuffix: '%',
               xDateFormat: '%Y-%m-%d'
           }
-      }]
+      }],
+      xAxis: {
+        labels: {
+          style: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+          }
+        },
+      },
+      yAxis: {
+        labels: {
+          style: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+          }
+        }
+      }
     }
 
     this.fnConfig = {   //init config for function usage chart
+      chart: {
+        borderColor: '#e6e6e8',
+        borderWidth: 1,
+        borderRadius: 2,
+        spacing: [16,16,16,16]
+      },
+      colors: ['#54A0FF', '#1DD1A1', '#FECA57', '#FF6B6B', '#FF9FF3', '#5F27CD', '#48DBFB', '#00D2D3'],
       loading: {
-        showDuration: 500
+        showDuration: 500,
+        labelStyle: {
+          fontFamily: 'IBM Plex Mono, sans-serif',
+          fontWeight: 400,
+          color: '#383840'
+        }
       },
-      title: {
-        text: 'Contract Function Calls'
+      rangeSelector: {
+        buttonTheme: {
+          fill: '#f6f6f8',
+          style: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+          },
+          states: {
+            hover: {
+              fill: '#0080ff',
+              style: {
+                color: 'white'
+              }
+            },
+            select: {
+              fill: '#54A0FF',
+              r: 1,
+              style: {
+                color: 'white'
+              }
+            }
+          }
+        },
+        inputStyle: {
+          fontFamily: 'IBM Plex Mono, sans-serif',
+          color: '#383840'
+        },
+        inputBoxBorderColor: '#e6e6e8',
+        labelStyle: {
+          fontFamily: 'IBM Plex Mono, sans-serif',
+          color: '#383840'
+        }
       },
-      series: []
+      scrollbar: {
+        barBackgroundColor: '#f6f6f8',
+        barBorderColor: 'e6e6e8',
+        buttonArrowColor: '#383840',
+        buttonBackgroundColor: '#f6f6f8',
+        buttonBorderColor: '#e6e6e8',
+        trackBackgroundColor: '#e6e6e8',
+        trackBorderColor: '#e6e6e8'
+      },
+      series: [],
+      xAxis: {
+        labels: {
+          style: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+          }
+        },
+      },
+      yAxis: {
+        labels: {
+          style: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+          }
+        }
+      }
     }
     
     this.state = {
