7817ed3f02c8442c44dd61cde4c6d73f0e3d20aa
Stedman Halliday <stedman.rh@gmail.com>
2018-04-25 21:25:03 -0700

    all chart styles

--- client/src/components/ChartConfig.js
+++ client/src/components/ChartConfig.js
@@ -1,87 +1,118 @@
 let ChartConfig = {   //init config for contract usage chart
     chart: {
-    borderColor: '#e6e6e8',
-    borderWidth: 1,
-    borderRadius: 2,
-    spacing: [16,16,16,16]
+        borderColor: '#e6e6e8',
+        borderWidth: 1,
+        borderRadius: 2,
+        spacing: [16, 16, 16, 16]
     },
     colors: ['#54A0FF', '#1DD1A1', '#FECA57', '#FF6B6B', '#FF9FF3', '#5F27CD', '#48DBFB', '#00D2D3'],
     loading: {
-    showDuration: 500,
-    labelStyle: {
-        fontFamily: 'IBM Plex Mono, sans-serif',
-        fontWeight: 400,
-        color: '#0080ff'
-    }
+        showDuration: 500,
+        labelStyle: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            fontWeight: 400,
+            color: '#0080ff'
+        }
     },
     rangeSelector: {
-    buttonTheme: {
-        fill: '#f6f6f8',
-        r: 1,
-        style: {
-        fontFamily: 'IBM Plex Mono, sans-serif',
-        color: '#383840'
-        },
-        states: {
-        hover: {
-            fill: '#0080ff',
+        buttonTheme: {
+            fill: '#f6f6f8',
+            r: 1,
             style: {
-            color: 'white'
+                fontFamily: 'IBM Plex Mono, sans-serif',
+                color: '#383840'
+            },
+            states: {
+                hover: {
+                    fill: '#0080ff',
+                    style: {
+                        color: 'white'
+                    }
+                },
+                select: {
+                    fill: '#54A0FF',
+                    style: {
+                        color: 'white'
+                    }
+                }
             }
         },
-        select: {
-            fill: '#54A0FF',
-            style: {
-            color: 'white'
-            }
-        }
+        inputStyle: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
+        },
+        inputBoxBorderColor: '#e6e6e8',
+        labelStyle: {
+            fontFamily: 'IBM Plex Mono, sans-serif',
+            color: '#383840'
         }
     },
-    inputStyle: {
-        fontFamily: 'IBM Plex Mono, sans-serif',
-        color: '#383840'
-    },
-    inputBoxBorderColor: '#e6e6e8',
-    labelStyle: {
-        fontFamily: 'IBM Plex Mono, sans-serif',
-        color: '#383840'
-    }
+    navigator: {
+        handles:{
+            backgroundColor: '#f6f6f8',
+            borderColor: '#bebec0'
+        },
+        margin: 8,
+        maskFill: 'rgba(84,160,255,.15)',
+        xAxis: {
+            gridLineColor: '#bebec0',
+            labels: {
+                style: {
+                    fontFamily: 'IBM Plex Mono, sans-serif',
+                    color: '#bebec0'
+                }
+            },
+            lineColor: '#e6e6e8'
+        }
     },
     scrollbar: {
-    barBackgroundColor: '#f6f6f8',
-    barBorderColor: 'e6e6e8',
-    buttonArrowColor: '#383840',
-    buttonBackgroundColor: '#f6f6f8',
-    buttonBorderColor: '#e6e6e8',
-    trackBackgroundColor: '#e6e6e8',
-    trackBorderColor: '#e6e6e8'
+        barBackgroundColor: '#f6f6f8',
+        barBorderColor: '#bebec0',
+        buttonArrowColor: '#383840',
+        buttonBackgroundColor: '#f6f6f8',
+        buttonBorderColor: '#bebec0',
+        rifleColor: '#bebec0',
+        trackBackgroundColor: '#e6e6e8',
+        trackBorderColor: '#e6e6e8'
     },
-    series: [
-    {
-    data: [], 
-    name: 'Volume',
-    tooltip: {
+    series: [{
+        data: [],
+        name: 'Volume',
+        tooltip: {
             valueDecimals: 2,
             valueSuffix: '%'
         }
     }],
-    tooltip: {xDateFormat: '%Y-%m-%d'},
-    xAxis: {
-    labels: {
+    tooltip: { 
+        backgroundColor: 'rgba(255,255,255,.85)',
+        borderRadius: 2,
+        shadow: false,
         style: {
-        fontFamily: 'IBM Plex Mono, sans-serif',
-        color: '#383840'
-        }
+            fontFamily: 'IBM Plex Sans, sans-serif',
+            color: '#383840'
+        },
+        xDateFormat: '%Y-%m-%d' 
     },
-    lineColor: 'white'
+    xAxis: {
+        crosshair: {
+            color: '#bebec0'
+        },
+        labels: {
+            style: {
+                fontFamily: 'IBM Plex Mono, sans-serif',
+                color: '#383840'
+            }
+        },
+        lineColor: '#e6e6e8'
     },
     yAxis: {
-    labels: {
-        style: {
-        fontFamily: 'IBM Plex Mono, sans-serif',
-        color: '#383840'
+        gridLineColor: '#e6e6e8',
+        labels: {
+            style: {
+                fontFamily: 'IBM Plex Mono, sans-serif',
+                color: '#383840'
+            }
         }
-    }
     }
 }
 
