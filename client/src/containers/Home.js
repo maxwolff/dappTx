7d1958cf1d2e64bce33995ffd4158c5a616fdd5d
Stedman Halliday <stedman.rh@gmail.com>
2018-04-28 10:23:25 -0700

    chart loading

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -13,18 +13,19 @@ class Home extends Component {
             address: '',
             exampleAddress: '0x06012c8cf97BEaD5deAe237070F9587f8E7A266d',
             volConfig: cloneDeep(ChartConfig),
-            fnConfig: cloneDeep(ChartConfig)
+            fnConfig: cloneDeep(ChartConfig),
+            isEmpty: true
         }
 
         this.volChart = React.createRef()    //chart component refs for imperative loading animations
         this.fnChart = React.createRef()
     }
 
-    callApi = async (newAddress) => {
+    callApi = async newAddress => {
         const url = '/api/' + newAddress + '/0x5A1340E0/0x5A8F969F'
         console.log(url)
 
-        axios.get(url).then((response) => {   //call API with URL to get data
+        axios.get(url).then(response => {   //call API with URL to get data
             const data = response.data
 
             const volSeries = Object.keys(data).map(elem => {   //map contract data for chart series config
@@ -58,26 +59,31 @@ class Home extends Component {
             const newFnConfig = cloneDeep(this.state.fnConfig)
             newFnConfig.series = fnSeries
 
-            this.setState({   //set state with new chart configs and address
-                address: newAddress,
-                volConfig: newVolConfig,
+            this.setState({
+                volConfig: newVolConfig,    //update charts with new configs
                 fnConfig: newFnConfig
             })
 
-        }).catch(function (error) {
+            this.volChart.current.hideLoading()     //hide loading states post-render
+            this.fnChart.current.hideLoading()
+
+        }).catch(error => {
             console.log(error)
         })
     }
 
-    renderAddress = (newAddress) => {
+    renderAddress = newAddress => {
         this.volChart.current.showLoading()     //trigger loading states before rerender
         this.fnChart.current.showLoading()
 
+        this.setState({address: newAddress})    //update address state and address bar input text
         this.callApi(newAddress)   //get chart data for a new address
     }
 
-    loadExample = () => {
+    loadExample = event => {
+
         this.renderAddress(this.state.exampleAddress)
+        this.setState({isEmpty: false})
     }
 
     render() {
@@ -85,8 +91,8 @@ class Home extends Component {
             <div>
                 <AppBar address={this.state.address} enterAddress={this.renderAddress} />
                 <main className="charts">
-                    <StockChart ref={this.volChart} config={this.state.volConfig} title="Contract Volume" subtitle="(percentage of sampled Ethereum transactions)" loadExample={this.loadExample} />
-                    <StockChart ref={this.fnChart} config={this.state.fnConfig} title="Contract Function Calls" loadExample={this.loadExample} />
+                    <StockChart ref={this.volChart} config={this.state.volConfig} title="Contract Volume" subtitle="(percentage of sampled Ethereum transactions)" isEmpty={this.state.isEmpty} loadExample={this.loadExample} />
+                    <StockChart ref={this.fnChart} config={this.state.fnConfig} title="Contract Function Calls" isEmpty={this.state.isEmpty} loadExample={this.loadExample} />
                 </main>
             </div>
         )
